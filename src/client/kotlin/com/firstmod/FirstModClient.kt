064a85fccff2681033e98a60ba89373f85a34b2a
santtuniskanen <santtuniska02@gmail.com>
2024-02-03 12:00:14 +0200

    init

---
+++ src/client/kotlin/com/firstmod/FirstModClient.kt
@@ -0,0 +1,9 @@
+package com.firstmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object FirstModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
